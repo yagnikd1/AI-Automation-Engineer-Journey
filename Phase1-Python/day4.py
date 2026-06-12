29edc97e7c09c929b254aa4013fbadc057fb094b
Yagnik <yagnikdudharejiya0@gmail.com>
2026-06-12 10:47:46 +0530

    Strings - 2

---
+++ Phase1-Python/day4.py
@@ -0,0 +1,48 @@
+# --------------------  Strings --------------
+
+name = 'Aahana'
+print(name.upper())
+print(name.lower())
+print(len(name))
+
+# -------------------  String Concatenation
+
+first_name = 'Aahana'
+last_name = 'Yagnik'
+
+full_name = first_name + ' '+ last_name
+print(full_name)
+
+# -------------------   Mini Project
+
+name = input("Enter your name: ")
+print('Welcome', name)
+print('your name contains', len(name), 'letters')
+
+# --------------------  Programs from Memory
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+print('hello', name)
+print('youre', name, 'and youre', age, 'years old' )
+
+# --------------------  2nd
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+occupation = input('Enter your occupation: ')
+workplace = input('Enter you workplace: ')
+address = input('Enter your Address: ')
+print('Hello', name, 'youre', age, 'years old and youre', occupation, 'you work in', workplace, 'your hometown is', address )
+
+
+
+
+
+
+
+
+
+
+
+
