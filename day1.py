027c2ec0f4d735a8eb15e7554080706596804d49
Yagnik <yagnikdudharejiya0@gmail.com>
2026-06-12 09:53:00 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
