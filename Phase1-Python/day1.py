0b6964ab1ec391ae7acae3a0d65218431e19d513
Yagnik <yagnikdudharejiya0@gmail.com>
2026-06-12 10:25:09 +0530

    Add files via upload

---
+++ Phase1-Python/day1.py
@@ -0,0 +1,28 @@
+# ------------------------- Name and age input programs ----------------
+
+
+#--------------- Excercise 1
+
+name = 'Aahana'
+age = 20
+print(name)
+print(age)
+name = input('what is your name?')
+print('hello', name)
+
+
+# -------------- Excercise 2
+
+
+
+name = 'Yagnik'
+age = 30
+height_ft = 5.9
+is_student = "NO"
+hobbies = ["games", "movies", "food"]
+
+print(name, type(name))
+print(age, type (age))
+print(height_ft, type(height_ft))
+print(is_student, type(is_student))
+print(hobbies, type(hobbies))
