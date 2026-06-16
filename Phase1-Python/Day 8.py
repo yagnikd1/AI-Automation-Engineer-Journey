5398c6f8551785170eed7044c757ed257fd0508e
Yagnik <yagnikdudharejiya0@gmail.com>
2026-06-16 10:18:51 +0530

    Memory Challenge -

---
+++ Phase1-Python/Day 8.py
@@ -0,0 +1,50 @@
+# -------------------  Program 1 - Personal Information
+
+#name = input("Enter Name: ")
+#age = input("Enter Age: ")
+#city = input("City: ")
+
+#print(name)
+#print(age)
+#print(city)
+
+
+# ------------------- Program 2 - Salary Countdown
+
+#salary = 15000
+#bonus = 2000
+
+#print('Total Salary', salary + bonus)
+
+
+# -------------------  Program 3 - Strings
+
+#name = 'Aahana'
+
+#print(name.upper())
+#print(name.lower())
+#print(name.title())
+
+
+# -------------------   Program 4 - Cities List
+
+#cities = ["Morbi", "Rajkot", "Ahmedabad"]
+
+#for city in cities:
+#    print(city)
+
+
+
+# -------------------   Memory challange
+
+name = input("Ente your Name: ")
+age = 20
+city = 'Morbi'
+
+print("Employee Information")
+print(name)
+print("Your name in UPPERCASE: ", name.upper())
+print(age)
+print(city)
+
+
