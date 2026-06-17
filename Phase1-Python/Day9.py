d2535b5abae93075a5a699248a0ba3c545cd3836
Yagnik <yagnikdudharejiya0@gmail.com>
2026-06-17 10:32:54 +0530

    List basics -

---
+++ Phase1-Python/Day9.py
@@ -0,0 +1,63 @@
+# ------------------------------------------------------- List ------------------------------------------
+
+# ------------------------- Accessing Items ----------------
+
+employees = ["Rahul", "Amit", "Neha", "Priya"]
+
+print(employees[0])
+
+print(employees[1])
+
+print(employees[2])
+
+print(employees[3])
+
+
+
+
+
+# ---------------------------------- List challenges ---------
+
+
+# ---------------------------- Challenge - 1
+
+cities = ["Morbi", "Rajkot", "Surat"]
+
+print(cities)
+
+print(cities[0])
+
+print(cities[2])
+
+print(cities[2])
+
+
+
+
+# ---------------------------- Challenge - 2
+
+employees = ["Rahul", "Amit", "Neha", "Priya"]
+
+print(employees[0])
+print(employees[2])
+
+
+
+
+# ---------------------------- Challenge - 3
+
+employees = ["Rahul", "Amit", "Neha", "Priya"]
+
+for employee in employees:
+    print(employee)
+    
+
+
+
+# ---------------------------- Challenge - 4
+
+cities = ["Morbi", "Rajkot", "Surat"]
+
+cities.append("Ahmedabad")
+
+print(cities)
